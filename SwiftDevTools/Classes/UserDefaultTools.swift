9bc0206aab3c1bb47cd30e8de8a0e2ce54e237a1
miaolingzi <645318955@qq.com>
2017-07-14 09:43:29 +0800

    fix UserDefaultTools and Release v0.0.5.4

--- SwiftDevTools/Classes/UserDefaultTools.swift
+++ SwiftDevTools/Classes/UserDefaultTools.swift
@@ -13,63 +13,63 @@ public class UserDefaultTools {
         return UserDefaults.standard
     }
     
-    class func putIntValue(key : String , value : Int) {
+    public class func putIntValue(key : String , value : Int) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func putFloatValue(key : String , value : Float) {
+    public class func putFloatValue(key : String , value : Float) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func putDoubleValue(key : String , value : Double) {
+    public class func putDoubleValue(key : String , value : Double) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func putBoolValue(key : String , value : Bool) {
+    public class func putBoolValue(key : String , value : Bool) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func putUrlValue(key : String , value : URL) {
+    public class func putUrlValue(key : String , value : URL) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func putAnyValue(key : String , value : Any) {
+    public class func putAnyValue(key : String , value : Any) {
         userDefaults.set(value, forKey: key)
     }
     
-    class func getIntValue(key : String) -> Int {
+    public class func getIntValue(key : String) -> Int {
         return userDefaults.integer(forKey: key)
     }
     
-    class func getFloatValue(key : String) -> Float {
+    public class func getFloatValue(key : String) -> Float {
         return userDefaults.float(forKey: key)
     }
     
-    class func getDoubleValue(key : String) -> Double {
+    public class func getDoubleValue(key : String) -> Double {
         return userDefaults.double(forKey: key)
     }
     
-    class func getBoolValue(key : String) -> Bool {
+    public class func getBoolValue(key : String) -> Bool {
         return userDefaults.bool(forKey: key)
     }
     
-    class func getArrayValue(key : String) -> [Any] {
+    public class func getArrayValue(key : String) -> [Any] {
         return userDefaults.array(forKey: key)!
     }
     
-    class func getDataValue(key : String) -> Data {
+    public class func getDataValue(key : String) -> Data {
         return userDefaults.data(forKey: key)!
     }
     
-    class func getDictValue(key : String) -> [String : Any] {
+    public class func getDictValue(key : String) -> [String : Any] {
         return userDefaults.dictionary(forKey: key)!
     }
     
-    class func getAnyValue(key : String) -> Any {
+    public class func getAnyValue(key : String) -> Any {
         return userDefaults.object(forKey: key)!
     }
     
-    class func removeObject(key : String) {
+    public class func removeObject(key : String) {
         userDefaults.removeObject(forKey: key)
     }
     
